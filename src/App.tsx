d3c08d913a359df3a5cc32995120d8580fe8e856
Зозуля Сергей <SZozulya@lanit.ru>
2022-01-28 20:00:23 +0300

    rectangle has been added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header/header";
 import Content from "./components/Content/Content";
+import Rectangle from "./components/Rectangle";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <div className={"wrapper"}>
                 <Header/>
                 <Content/>
+                <Rectangle/>
                 <footer className={"footer"}>
                     Footer
                 </footer>
---
+++ src/components/Rectangle.tsx
@@ -0,0 +1,8 @@
+import s from "./Content/rectangle.module.css"
+const Rectangle = () => {
+    return <div className={s.Rectangle}>
+
+    </div>
+}
+
+export default Rectangle
